8fdd3adf8799de77ac74fc937b519fec1cd6ad7c
MarcoFilippozzi <marco.filippozzi98@gmail.com>
2017-04-29 17:58:37 +0200

    Added API request

---
+++ ConsoleApplication2/Observation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace ConsoleApplication2
+{
+    class Observation
+    {
+        public string id;
+        public string latitude;
+        public string longitude;
+        public string created_at;
+        public string place_guess;
+        public string species_guess;
+        public string quality_grade;
+        public string num_identification_disagreements;
+
+        [JsonConstructor]
+        public Observation(string id, string latitude, string longitude, string created_at, string place_guess, string species_guess, string quality_grade, string num_identification_disagreements)
+        {
+            this.id = id;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.created_at = created_at;
+            this.place_guess = place_guess;
+            this.species_guess = species_guess;
+            this.quality_grade = quality_grade;
+            this.num_identification_disagreements = num_identification_disagreements;
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using Newtonsoft.Json;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public static string url = "https://www.inaturalist.org/";
+        public static string observationParams = "observations.json?per_page=200";
+        public static string placesParams = "places.json?per_page=200&place_type=country";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert country: ");
+            string country = Console.ReadLine();
+            
+            string text;
+            List<Observation> obs = new List<Observation>();
+            
+            var request = WebRequest.Create(url + placesParams);
+            request.ContentType = "application/json; charset=utf-8";
+
+            var response = (HttpWebResponse)request.GetResponse();
+            List<Place> places;
+            Place countrySelected = new Place();
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                text = sr.ReadToEnd();
+                places = JsonConvert.DeserializeObject<List<Place>>(text);
+                
+                foreach(Place place in places)
+                {
+                    if(place.name == country)
+                    {
+                        countrySelected = place;
+                    }
+                }
+            }
+            int page = 0;
+            while (true)
+            {
+                request = WebRequest.Create(url + observationParams + "&swlat=" + countrySelected.swlat + "&swlng=" + countrySelected.swlng + "&nelat=" + countrySelected.nelat + "&nelng=" + countrySelected.nelng + "&page=" + page);
+                request.ContentType = "application/json; charset=utf-8";
+                response = (HttpWebResponse)request.GetResponse();
+
+
+                using (var sr = new StreamReader(response.GetResponseStream()))
+                {
+                    text = sr.ReadToEnd();
+                    List<Observation> tmp = JsonConvert.DeserializeObject<List<Observation>>(text);
+                    obs.AddRange(tmp);
+
+                }
+                page++;
+            }
+            
+            
+
+
+        }
+    }
+}
